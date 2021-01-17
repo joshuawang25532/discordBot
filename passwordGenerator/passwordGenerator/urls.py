a8d794f23fbe61b790991815f50994a66d132bea
BuildTools <unconfigured@null.spigotmc.org>
2021-01-16 21:40:22 -0600

    'test'

--- passwordGenerator/passwordGenerator/urls.py
+++ passwordGenerator/passwordGenerator/urls.py
@@ -17,6 +17,13 @@ Including another URLconf
 from django.urls import path
 from generator import views
 
+
+
+
+
+
+
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('password/', views.password, name='password'),
